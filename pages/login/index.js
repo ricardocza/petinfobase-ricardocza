b92fccc5df3b0cd526fdef0ea1d59b755e1a0588
Ricardo do Vale <ricardovcza@gmail.com>
2022-10-19 12:08:21 -0300

    init API

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,8 @@
-/* Desenvolva seu código aqui */
+import { loginAPI } from "../../scripts/api.js";
+
+let body = {
+    email: 'rafael@mail.com',
+    password: '123456'
+}
+
+await loginAPI(body)
--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,20 @@
-/* Desenvolva seu código aqui */
+const baseURL = 'http://localhost:3333/'
+
+async function loginAPI(body) {
+    try {
+        const login = await fetch(`${baseURL}login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await login.json()
+        console.log(await response)
+    } catch (err) {
+        console.log(err)
+
+    }
+}
+
+export {loginAPI}
